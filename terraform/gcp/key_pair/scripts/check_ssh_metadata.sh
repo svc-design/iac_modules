fc8e6f2e6465713e97ea4ebdf87590e7ff452602
Haitao Pan <manbuzhe2009@qq.com>
2024-02-08 15:30:51 +0800

    fixed run scripts/check_ssh_metadata.sh

--- terraform/gcp/key_pair/scripts/check_ssh_metadata.sh
+++ terraform/gcp/key_pair/scripts/check_ssh_metadata.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+export config=$1 
 # Get the directory of the script
 script_dir="$(dirname "$(readlink -f "$0")")"
 
 # Read config.yaml and set environment variables
-project_id=$(yq eval '.project_id' "$script_dir/../../config.yaml")
-key_pairs_name=$(yq eval '.key_pairs[0].name' "$script_dir/../../config.yaml")
+project_id=$(yq eval '.project_id' "$script_dir/../../${config}")
+key_pairs_name=$(yq eval '.key_pairs[0].name' "$script_dir/../../${config}")
 
 if [ -z "$project_id" ]; then
   echo "Error: Project ID not set in config.yaml"
